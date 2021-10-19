ee68d42f86a89f827ea32e59a3137b88efc2bcf3
navroopsinghsandhu <navroops806@gmail.com>
2021-10-19 22:17:58 +0530

    Native API and data communication to the javsscript interface
    Sends mTimeNanos to the project's index.js

--- www/gnssclocklugin.js
+++ www/gnssclocklugin.js
@@ -3,11 +3,11 @@ function GnssclockPlugin() {}
 
 // The function that passes work along to native shells
 // Message is a string, duration may be 'long' or 'short'
-GnssclockPlugin.prototype.show = function(message, duration, successCallback, errorCallback) {
+GnssclockPlugin.prototype.getgpsTime = function(message, duration, successCallback, errorCallback) {
   var options = {};
   options.message = message;
   options.duration = duration;
-  cordova.exec(successCallback, errorCallback, 'GnssclockPlugin', 'show', [options]);
+  cordova.exec(successCallback, errorCallback, 'GnssclockPlugin', 'getTime', [options]);
 }
 
 // Installation constructor that binds GnssclockPlugin to window
